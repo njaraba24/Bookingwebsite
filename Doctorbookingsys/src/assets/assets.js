dfe7e4985b42b1555e0480ac47437f6451940bb8
Evans <160320830+njaraba24@users.noreply.github.com>
2025-03-06 08:20:46 +0300

    Update assets.js

--- Doctorbookingsys/src/assets/assets.js
+++ Doctorbookingsys/src/assets/assets.js
@@ -15,6 +15,7 @@ import verified_icon from './verified_icon.svg'
 import arrow_icon from './arrow_icon.svg'
 import info_icon from './info_icon.svg'
 import upload_icon from './upload_icon.png'
+import upload_area from './upload_area.png'
 import stripe_logo from './stripe_logo.png'
 import razorpay_logo from './razorpay_logo.png'
 import doc1 from './doc1.png'
@@ -60,7 +61,8 @@ export const assets = {
     dropdown_icon,
     upload_icon,
     stripe_logo,
-    razorpay_logo
+    razorpay_logo,
+    upload_area
 }
 
 export const specialityData = [
