58ab554ea991516b7ef048a66810d531a4d7f271
Evans <160320830+njaraba24@users.noreply.github.com>
2025-03-27 14:50:28 +0300

    Update Home.jsx

--- Doctorbookingsys/src/pages/Home.jsx
+++ Doctorbookingsys/src/pages/Home.jsx
@@ -6,6 +6,22 @@ import TopDoctors from '../components/TopDoctors';
 import Banner from '../components/Banner';
 
 const Home = () => {
+   useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = "https://cdn.jsdelivr.net/npm/@denserai/embed-chat@1/dist/web.min.js";
+    script.onload = () => {
+      window.Chatbot.init({
+        chatbotId: "e14fec1a-3b9b-469c-8c45-27c97a15c1b1",
+      });
+    };
+    document.body.appendChild(script);
+    
+    // Cleanup function to remove the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <div>
       <Header />
